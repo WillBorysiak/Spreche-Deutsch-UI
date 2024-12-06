58e766a0b9b5cb2e20d1bb0578b12256ab1d6e44
Will Borysiak <will.gains@icloud.com>
2024-12-06 10:58:26 +0000

    shadcn added to project and input migrated

--- src/components/content/translations/TranslationSearch.tsx
+++ src/components/content/translations/TranslationSearch.tsx
@@ -1,6 +1,6 @@
 import { Dispatch } from "react";
 
-import { Input } from "@headlessui/react";
+import { Input } from "@/lib/shadcn/ui/input";
 
 interface SortMenuProps {
   searchSelection: Dispatch<string>;
@@ -13,10 +13,7 @@ const TranslationSearch = (props: SortMenuProps) => {
     <div id="translation-search" className="flex flex-row items-center">
       <p className="mr-2 text-lg font-bold sm:text-xl"> Search:</p>
 
-      <Input
-        className="w-full cursor-text rounded-sm bg-zinc-300 px-3 py-1 text-lg text-white focus:outline-none dark:bg-zinc-800"
-        onChange={(e) => searchSelection(e.target.value)}
-      />
+      <Input onChange={(e) => searchSelection(e.target.value)} />
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,27 @@
 
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class",
+  darkMode: ["class"],
   theme: {
     extend: {
       screens: {
         "3xl": "1600px",
       },
-      maxWidth: { "8xl": "1600px" },
+      maxWidth: {
+        "8xl": "1600px",
+      },
       fontFamily: {
         rosario: ["Rosario", "san-serif"],
       },
       colors: {
         transparentBg: "rgba(0, 0, 0, 0.2)",
       },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
